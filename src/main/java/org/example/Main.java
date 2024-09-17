2bf06d8bcd6b535072c52914f4065f95e7beb909
Vitaly <vivseev02@inbox.ru>
2024-09-17 23:08:57 +0400

    added conversion of GeoJson file to List<>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import org.geojson.LngLatAlt;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<LngLatAlt> geoJsonArray;
+
+        try {
+            geoJsonArray = GeoJsonManager.loadGeoJson("src/main/resources/GeoJsonMap1");
+        }
+        catch (IOException e){
+            
+        }
     }
 }
